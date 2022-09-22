e5ac8745e8db6f3c5655a96d6497fca2a1ed930c
Takanori Suzuki <takanori@takanory.net>
2022-09-22 21:56:39 +0900

    ISSHA-2573 update tshirts 2022 code

--- tshirts2022.py
+++ tshirts2022.py
@@ -6,12 +6,12 @@ URL = "https://www.pycon.jp/support/bootcamp.html"
 with request.urlopen(URL) as f:
     html = f.read().decode("utf-8")
 pattern = re.compile(r"pyconjp.*/event/(\d+)/(?!.*中止)")
-ids = ",".join(m[1] for m in pattern.finditer(html))
+ids = ",".join(pattern.findall(html))
 
 connpass_api = "https://connpass.com/api/v1/event/"
 url = f"{connpass_api}?event_id={ids}&count=100"
 with request.urlopen(url) as f:
-    data = json.loads(f.read())
+    data = json.load(f)
 
 features = [{
     "type": "Feature",
@@ -21,9 +21,10 @@ features = [{
     },
     "properties": {"name": e["title"]},
 } for e in data["events"]]
-geo = {"type": "FeatureCollection",
-       "features": features}
+geo = {"type": "FeatureCollection", "features": features}
 
 with open("pycamp.geojson", "w") as f:
     json.dump(geo, f, indent=2, ensure_ascii=False)
 print("Number of events:", len(features))  # -> 45
+
+print("It's your turn.")
